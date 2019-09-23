3c0aaf9b0f49f90090317dbef47e44f473abe29e
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-09-23 14:41:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+//for case insensitive must convert to uppercase
+
+
+function findMatching(drivers, string) {
+    //console.log("string passed === ", string)
+    return drivers.filter( driver => {
+        return driver.toUpperCase() == string.toUpperCase()
+    })
+};
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter( driver => {
+        return driver.charAt(0) === string.charAt(0)
+    })
+}
+
+function matchName(drivers, string) {
+    return drivers.filter( driver => { 
+        return driver.name === string
+    })
+};
